83dac961907227ef6135c7cac79c1670cc7433f9
Rafael Gabriel <rafaeldzn2003@poli.ufrj.br>
2023-09-23 13:51:45 -0300

    Criando listConfig, para manipular os dados da netlist mais facilmente

---
+++ trab1rafaelgabriel.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+# A funcao abaixo foi criado com para eu dispor os dados da netlist de uma maneira que acho mais simples de trabalhar
+def listConfig(nomeArq):
+    arqNetlist = open(nomeArq + '.txt', 'r')
+    linhaNetlist = arqNetlist.readline()
+    listaConfig = []
+
+    while (linhaNetlist != ''):
+        auxList = []
+        auxList2 = []
+        
+        # Fazendo a organizacao da lista
+        # De maneira resumida, o que estou fazendo é separar cada linha da netlist em uma linha da minha matriz de dados
+        # E em cada linha, os dados estao despostos de uma maneira mais intuitica
+        for caracter in linhaNetlist:
+            if (caracter != ' '):
+                auxList.append(caracter)
+
+            elif (caracter == ' '):
+                auxList2.append(''.join(auxList))
+                auxList = []
+
+        listaConfig.append(auxList2)
+        linhaNetlist = arqNetlist.readline()
+    arqNetlist.close()
+    #print(listaConfig)
+
+    return listaConfig
